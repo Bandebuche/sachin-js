036d1bb35dea52b58c2aac98b96291b73289fdc1
Bandebuche <127002087+Bandebuche@users.noreply.github.com>
2025-08-26 09:54:59 +0000

    datatypes

---
+++ js_sachin/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS Code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3 ) //code readabilty should be high
+
+console.log("sachin")
+
+let name = "sachin"
+let age = 18
+let isLoggedIn = false
+
+//  number => 2 to power 53
+//  bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+
+console.log(typeof "sachin")
