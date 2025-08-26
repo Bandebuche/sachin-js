125f0c43ccfce6cdea5089791bcfc785a00a36b0
Bandebuche <127002087+Bandebuche@users.noreply.github.com>
2025-08-26 09:35:49 +0000

    datatypes

--- basic_1/02_datatypes.js
+++ basic_1/02_datatypes.js
@@ -7,3 +7,20 @@ console.log(3
     3 ) //code readabilty should be high
 
 console.log("sachin")
+
+let name = "sachin"
+let age = 18
+let isLoggedIn = false
+
+//  number => 2 to power 53
+//  bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+
+console.log(typeof "sachin")
