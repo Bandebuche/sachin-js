c1536c3edb93885e5d4a3612136f8f6a1cc95f3e
Bandebuche <127002087+Bandebuche@users.noreply.github.com>
2025-08-26 08:20:33 +0000

    datatypes

---
+++ basic_1/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";  // treat all JS Code as newer version
+
+// alert ( 3 + 3 ) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3 ) //code readabilty should be high
+
+console.log("sachin")
